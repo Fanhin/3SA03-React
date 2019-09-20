2c6968dc569b630cb025da29e43d27685ff1a2b3
Fanhin <6010110684@psu.ac.th>
2019-09-21 02:25:26 +0700

    do shuffle card

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 //import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-//const word = "Hello"
+const word = "Hello"
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WordCard value="hello"/>
+        <WordCard value={word} />
         
         
       </div>
--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class CharacterCard extends Component {
+
+    
     constructor(props){
         super(props)
             this.state={
                 active:false,
+            }
+        }
+
+        componentDidUpdate(prevProps){
+            if (prevProps.attempt != this.props.attempt) {
+                this.setState({active : false})
             }
         }
 
@@ -16,13 +24,15 @@ export default class CharacterCard extends Component {
         }
     
     render() {
-        let className=`card ${this.state.active ? 'activeCard': ''}`
+        let className=`card ${this.state.active ? 'activeCard': '' }`
         return (
-            <div className={className} onClick={this.activate}>
-                {this.props.value}
-                
-                
+            <div >
+                <div className={className} onClick={this.activate}>
+                    {this.props.value}
+                </div>
+
             </div>
         )
     }
 }
+
